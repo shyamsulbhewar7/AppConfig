bfbd48846788b95f27b43e4fa7f1be5882b1d66d
shyamsulbhewar7 <shyamsulbhewar@gmail.com>
2022-06-24 18:56:04 +0530

    remaining files

---
+++ Model/ConfigRead.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace AppConfigV2.Model
+{
+    public class ConfigRead
+    {
+        public string GetServer()
+        {
+            return ConfigurationManager.AppSettings["server"];
+        }
+        public string GetDatabase()
+        {
+            return ConfigurationManager.AppSettings["database"];
+        }
+        public string GetConnection1()
+        {
+            return ConfigurationManager.AppSettings["Connection1"];
+        }
+        public string GetConnection2()
+        {
+            return ConfigurationManager.AppSettings["Connection2"];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using AppConfigV2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigRead config = new ConfigRead();
+            Console.WriteLine("{0}", config.GetConnection1());
+            Console.WriteLine("{0}", config.GetConnection1());
+            Console.WriteLine("");
+            Console.WriteLine("{0}", config.GetServer());
+            Console.WriteLine("{0}", config.GetDatabase());
+            Console.ReadKey();
+        }
+    }
+}
